61025c4d133bdd7e745f2672fc4d5b92cb4403c6
Akash B <akashb50@yahoo.co.uk>
2018-04-15 15:45:18 +0100

    - Search function changed to support GET and POST requests - Removed extend function: extend functionality now assimilated into search function

--- venv/application.py
+++ venv/application.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_jsglue import JSGlue
-from helpers import lookup, MAX_RESULTS
+from helpers import lookup, MAX_RESULTS, lookup
 from pagination import Pagination
 
 # configure application
@@ -11,7 +11,7 @@ JSGlue(app)
 @app.route("/", methods=['GET'])
 def index():
     """Render home-page."""
-    results = lookup(query="")
+    results = lookup(query="", offset=0)
     num_results = results.pop()  # number of results - not important
     headlines = results  # list of headlines
 
@@ -22,11 +22,27 @@ def index():
 @app.route("/search/<int:page_num>", methods=['GET', 'POST'])
 def search(page_num=1):
     """Search user query."""
-    offset = (page_num - 1) * MAX_RESULTS
 
-    query = request.args.get('q').strip()  # ensure query is some kind of string
+    if request.method == "GET":
+        query = request.args.get('q').strip()
+        results_per_page = request.args.get('select-results-per-page')
 
-    results = lookup(query=query, offset=offset, max_results=MAX_RESULTS)
+    else:
+        query = request.form['q'].strip()
+        results_per_page = request.form['select-results-per-page']
+
+    if not query:
+        query = ''
+
+    if not results_per_page:
+        results_per_page = MAX_RESULTS
+
+    else:
+        results_per_page = int(results_per_page)
+
+    offset = (page_num - 1) * results_per_page
+
+    results = lookup(query=query, offset=offset, max_results=results_per_page)
 
     num_results = results.pop()  # total matches on query string
     headlines = results  # list of headlines
@@ -36,31 +52,18 @@ def search(page_num=1):
 
     pages = Pagination(page_num, (num_results // MAX_RESULTS) + 1, 4, 4)
 
-    return render_template("search.html", query=query, headlines=headlines,
-                           num_results=num_results, pages=pages.paginate(), current_page=page_num,
-                           results_per_page=MAX_RESULTS)
+    if request.method == "GET":
+        return render_template("search.html", query=query, headlines=headlines,
+                               num_results=num_results, pages=pages.paginate(), current_page=page_num,
+                               results_per_page=results_per_page)
 
+    else:
+        show_more = True
+        if num_results < page_num * MAX_RESULTS:
+            show_more = False
 
-@app.route("/extend/<int:page_num>", methods=["POST"])
-def extend(page_num):
-    offset = (page_num - 1) * MAX_RESULTS
-    query = request.form['q'].strip()  # ensure query is some kind of string
-
-    results = lookup(query=query, offset=offset, max_results=MAX_RESULTS)
-
-    num_results = results.pop()     # total matches on query string
-    headlines = results             # list of headlines
-    show_more = True
-
-    if not headlines[0]:
-        headlines = None
-
-    if num_results < page_num * MAX_RESULTS:
-        show_more = False
-
-    return jsonify({'data': render_template("extend.html", headlines=headlines,
-                                            num_results=num_results),
-                    'extend': show_more})
+        return jsonify({'data': render_template("extend.html", headlines=headlines, num_results=num_results),
+                        'extend': show_more})
 
 
 if __name__ == '__main__':
