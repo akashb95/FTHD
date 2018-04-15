04a21e8dec2b0a9fe3bda949d099dbf2000e4197
Akash B <akashb50@yahoo.co.uk>
2018-04-15 15:39:03 +0100

    - Added dropdown menu for choosing number of results to show per page

--- venv/js/scripts.js
+++ venv/js/scripts.js
@@ -1,11 +1,39 @@
 $(document).ready(function () {
+    function updateResultsPerPage() {
+        resultsPerPage = parseInt($('#select-results-per-page option:selected').val());
+    }
+
+    let query = $('#q').val();
     let numberExtended = 0;     // Number of times headlines updated via AJAX.
     let currentPage = parseInt($('#page-number').text());
-    let resultsPerPage = parseInt($('#results-per-page').text());
+    let resultsPerPage = updateResultsPerPage();
     let totalArticles = parseInt($('#total-articles').text());
 
     // In the pagination section, indicate to user current page number.
     $('#' + currentPage).css({'text-color': 'black', 'font-weight': 'bold', 'font-size': '105%'});
+
+    // When user selects number of results to view per page from dropdown.
+    $(document).on('change', '#select-results-per-page', function() {
+        let oldResultsPerPage = resultsPerPage;
+        resultsPerPage = updateResultsPerPage();
+        let newPageNumber = ~~((currentPage * oldResultsPerPage) / resultsPerPage) + 1;
+        $.ajax({
+            url: "/search/" + newPageNumber,
+            method: "POST",
+            data: $('form').serialize(),
+            success: function (response) {
+                if (!response.extend) {
+                    $('#extend-button').prop('disabled', 'true');
+                    $('#extend-button-div').fadeOut(500);
+                }
+                $('#headlines-list').empty().append(response.data);
+                $('div.pagination a').each(function () {
+                    $(this).attr("href", "/search/" + $(this).attr('id') + "?q=" + query + "&select-results-per-page=" +
+                        parseInt($('#select-results-per-page option:selected').val()));
+                })
+            }
+        })
+    });
 
     // Hide extend button if no more articles found.
     if (currentPage * resultsPerPage > totalArticles) {
@@ -20,9 +48,9 @@ $(document).ready(function () {
         numberExtended++;
         let fetchPageNumber = currentPage + numberExtended;
         $.ajax({
-            url: "/extend/" + fetchPageNumber,
+            url: "/search/" + fetchPageNumber,
             type: "POST",
-            data: $('#q').serialize(),
+            data: $('form').serialize(),
             success: function (response) {
                 // If no more headlines on topic, disable button and hide it.
                 if (!response.extend) {
