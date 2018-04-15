662dd47809c50dce59d7a0cb1527a0b5a03cd2a3
Akash B <akashb50@yahoo.co.uk>
2018-04-15 15:42:29 +0100

    - Changed lookup function so that cached results are not displayed despite when new data from API received. - Cache removed - all responses are acquired directly from FT API for every query. - Minor changes to support variable number of results shown per page.

--- venv/helpers.py
+++ venv/helpers.py
@@ -1,7 +1,7 @@
 import requests
 from key import api_key
 
-MAX_RESULTS = 50                       # default number of results on page
+MAX_RESULTS = 50  # default number of results on page
 
 api = {"apiKey": api_key}
 
@@ -30,8 +30,9 @@ parameters = {"queryString": "",
 endpoint = "http://api.ft.com/content/search/v1?apiKey={}".format(api_key)
 
 
-def lookup(query, offset=None, max_results=None):
+def lookup(query, offset, max_results=None):
     """
+
     Looks up query articles from FT using FT Headlines API.
     :param query: [str] query string
     :param offset: [int] (pageNumber - 1) * max_results
@@ -39,48 +40,32 @@ def lookup(query, offset=None, max_results=None):
     :return: [List] list of articles with headlines, link to article, timestamp and a summary.
     """
 
-    # check cache for articles
-    if query in lookup.cache:
-        # check if cache contains articles for the right page number and the right number of results
-        if lookup.cache[query][-1]["offset"] == offset and lookup.cache[query][-1]["maxResults"] == max_results:
-            return lookup.cache[query][:-1]
+    parsed_articles = {}
 
-    # user-defined number of results per page
     if max_results:
         parameters["resultContext"]["maxResults"] = max_results
 
-    # offset as defined by FT API - which index of articles to begin showing results from
-    if offset:
-        parameters["resultContext"]["offset"] = offset
+    parameters["resultContext"]["offset"] = offset
 
-    # user-entered query string
-    parameters['queryString'] = query
+    parameters["queryString"] = query
+
     response = requests.post(endpoint, params=api, headers=headers, json=parameters).json()
 
-    # number of articles found
     index_count = response["results"][0]["indexCount"]
+
     try:
         articles = response["results"][0]["results"]
+        parsed_articles[query] = [{"link": item["location"]["uri"], "title": item["title"]["title"],
+                                   "summary": item["summary"],
+                                   "timestamp": parse_datetime(item["lifecycle"]["lastPublishDateTime"])}
+                                  for item in articles]
 
-        # cache results
-        lookup.cache[query] = [
-            {"link": item["location"]["uri"], "title": item["title"]["title"], "summary": item["summary"],
-             "timestamp": parse_datetime(item["lifecycle"]["lastPublishDateTime"])}
-            for item in articles]
-
-    # if no results found
     except KeyError:
-        lookup.cache[query] = [None]
+        parsed_articles[query] = [None]
 
-    lookup.cache[query].append(index_count)
-    lookup.cache[query].append({'maxResults': max_results, 'offset': offset})
+    parsed_articles[query].append(index_count)
 
-    # send everything but the result details
-    return lookup.cache[query][:-1]
-
-
-# initialize cache
-lookup.cache = {}
+    return parsed_articles[query]
 
 
 def parse_datetime(dt):
